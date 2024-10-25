a21c9336deabaa812e9e4906a3f0c91b3917d0d5
Gmarques023 <goncalom8484@gmail.com>
2024-10-25 16:30:47 +0100

    set up all token addresses

--- tokens.js
+++ tokens.js
@@ -8,56 +8,58 @@
 //raydium
 //pancakeswap
 
-//BNB
+//BNB --> check
 //USDC
 //WSTETH
-//WBTC
-//WETH
-//LINK
-//UNI
-//DAI
-//POL
-//AAVE
-//RENDER
-//INJ
-//BONK
-//The Graph
-//GRT
-//RETH
-//MANTRA
-//OM
-//LDO
-//MKR
-//MATIC
-//APE
+//WBTC --> check
+//WETH  --> check
+//LINK --> check
+//UNI  --> check
+//COMP --> check
+//DAI --> check
+//AAVE  --> check
+//RNDR  --> check
+//INJ   --> check
+//BONK  --> check
+//GRT  --> check
+//OM --> check
+//LDO  --> check
+//MKR   --> check
+//MATIC  --> check
+//APE --> check
 //AXL
 //FRAX
-//SAND
-//MANA
+//SAND --> check
+//MANA  --> check
+//SUSHI  --> check
+//SHIB  --> check
 
 const tokens = [
-  { name: "DAI", address: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063" },
-  { name: "WMATIC", address: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270" },
-  { name: "WETH", address: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619" },
+  { name: "DAI", address: "0x6B175474E89094C44Da98b954EedeAC495271d0F" },
+  { name: "WETH", address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" },
   { name: "WBTC", address: "0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6" },
-  { name: "LINK", address: "0x53E0bca35eC356BD5ddDFebbD1Fc0fD03FaBad39" },
-  { name: "SAND", address: "0x6B175474E89094C44Da98b954EedeAC495271d0F" },
-  { name: "AAVE", address: "0x9B2c8D74A2B98e582D0013b9f3A30d5D8B5d9Ff0" },
-  { name: "COMP", address: "0xc00e94Cb662C3520282E6f5717214004A7b007e" },
-  { name: "DAI", address: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063" },
-  { name: "BAL", address: "0xba100000625a377881b0fdadf5b0a3d4b68645e0" },
-  { name: "CRV", address: "0x172370d5cd63279e721c5f4b719b4f6e32d22c2b" },
-  { name: "FTM", address: "0x4e15361fcd9fe26e1b6f8a9c15a4c3b2c40e3c0b" },
-  { name: "SUSHI", address: "0x0b3F868E0BE5597D5DB7fEB59e1CADBc4D29E10" },
-  { name: "LTC", address: "0x51B2AB8C1A96A4eC6eDA3D1eFCBC3BCE7C0576BF" },
+  { name: "LINK", address: "0x514910771AF9Ca656af840dff83E8264EcF986CA" },
+  { name: "SAND", address: "0x3845badAde8e6dFF049820680d1F14bD3903a5d0" },
+  { name: "AAVE", address: "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9" },
+  { name: "COMP", address: "0xc00e94Cb662C3520282E6f5717214004A7f26888" },
+  { name: "DAI", address: "0x6B175474E89094C44Da98b954EedeAC495271d0F" },
+  { name: "SUSHI", address: "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2" },
   { name: "MANA", address: "0x0f5d2fb29fb7d3cfe444a200298f468908cc942" },
-  { name: "GRT", address: "0x6f3B7dA0BF45D6c7C6DBb7bC2000bD2fA8e15B8d" },
-  { name: "YFI", address: "0x0bc529c00C6401aEF6D220BE8C6Ea16619d9e9f5" },
-  { name: "FLOKI", address: "0x5B5cA68d9B70DAd2eA540F9B53c658BE00678E49" },
-  { name: "REN", address: "0x408e41876cCCDC0F92210600ef503726560A3F19" },
-  { name: "TWT", address: "0x4B50fC378BDA3b16532D3D2d1A12f93c3E4C2F02" },
-  { name: "SHIB", address: "0x95aD61b0a150d79219B9C5c9e8C946bE7A94b646" },
-  { name: "CRO", address: "0x1f1fDCE5390471C06276dF8B99D57E5f85FC4d0b" },
+  { name: "GRT", address: "0xc944E90C64B2c07662A292be6244BDf05Cda44a7" },
+  { name: "SHIB", address: "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE" },
+  { name: "BNB", address: "0xB8c77482e45F1F44dE1745F52C74426C631bDD52" },
+  { name: "UNI", address: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984" },
+  { name: "RNDR", address: "0x6De037ef9aD2725EB40118Bb1702EBb27e4Aeb24" },
+  { name: "INJ", address: "0xe28b3B32B6c345A34Ff64674606124Dd5Aceca30" },
+  { name: "BONK", address: "0x1151CB3d861920e07a38e03eEAd12C32178567F6" },
+  { name: "FTM", address: "0x4E15361FD6b4BB609Fa63C81A2be19d873717870" },
+  { name: "OM", address: "0x3593D125a4f7849a1B059E64F4517A86Dd60c95d" },
+  { name: "LDO", address: "0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32" },
+  { name: "MKR", address: "0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2" },
+  { name: "MATIC", address: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0" },
+  { name: "APE", address: "0x4d224452801ACEd8B2F0aebE155379bb5D594381" },
+  { name: "AXL", address: "0x467719aD09025FcC6cF6F8311755809d45a5E5f3" },
+  { name: "FRAX", address: "0x853d955aCEf822Db058eb8505911ED77F175b99e" },
 ];
 
 export default tokens;
